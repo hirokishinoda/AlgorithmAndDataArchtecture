69b4d5820453f96c3cdc5d5790f649b84792e24c
shinodahiroki <rss23843@nifty.com>
2019-05-30 20:55:26 +0900

    [追加]削除機能を追加．

--- binary_search_tree.c
+++ binary_search_tree.c
@@ -15,7 +15,7 @@ typedef struct NODE{
 void preorder(NODE *p){
   if(p == NULL) return;
 
-  printf("%d\n",p->val);
+  printf("%d t:%p r:%p l:%p\n",p->val,p,p->right,p->left);
   preorder(p->left);
   preorder(p->right);
 }
@@ -96,16 +96,77 @@ int search_node(NODE *root,int val){
   return -1;
 }
 
+/*
+ * 最小ノードを探す
+ * */
+NODE *min_node(NODE **p){
+  NODE *q;
+
+  while((*p)->left != NULL){
+    p = &(*p)->left;
+  }
+  q = *p;
+  *p = (*p)->right;
+
+  return q;
+}
+
+/*
+ * 削除
+ * */
+int remove_node(NODE **root,int val){
+  NODE **p,*x;
+
+  p = root;
+  while(p != NULL){
+    if((*p)->val == val){
+
+      x = *p;
+      if((*p)->right == NULL && (*p)->left == NULL){
+        *p = NULL;
+      }else if((*p)->right == NULL){
+        *p = (*p)->left;
+      }else if((*p)->left == NULL){
+        *p = (*p)->right;
+      }else{
+        *p = min_node(&(x->right));
+        (*p)->right = x->right;
+        (*p)->left = x->left;
+      }
+      free(x);
+
+      printf("removed %d\n.",val);
+      return 1;
+
+    }else if((*p)->val > val){
+      p = &(*p)->left;
+    }else{
+      p = &(*p)->right;
+    }
+  }
+  printf("Not Found.\n");
+  return 0;
+}
+
 int main(void){
   NODE *root = NULL;
 
-  insert_node(&root,1);
-  insert_node(&root,2);
+  insert_node(&root,5);
+  insert_node(&root,8);
   insert_node(&root,3);
+  insert_node(&root,2);
+  insert_node(&root,1);
+  insert_node(&root,4);
+  insert_node(&root,6);
+  insert_node(&root,9);
+  insert_node(&root,10);
 
   preorder(root);
 
   search_node(root,4);
+
+  remove_node(&root,8);
+  preorder(root);
 
   return 0;
 }
